5b4a312a1570d1de72e267e5ea663d3ed7897c80
code with srnagur <acchinagur@gmail.com>
2024-07-19 13:37:38 +0530

    push the project

---
+++ demopract/src/test/java/contactlastn/CreateContact.java
@@ -0,0 +1,11 @@
+package contactlastn;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("lastname successfully");
+
+	}
+
+}
